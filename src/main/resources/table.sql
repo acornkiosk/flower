2c3ff94a605719421a202562e8e0df3a494bc92f
unso99 <unso99@naver.com>
2024-02-05 15:22:51 +0900

    Fix: kiosk table 제약 위반 수정
    제약 이름 중복 수정

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -4,5 +4,5 @@ CREATE SEQUENCE seq_kiosk_id;
 CREATE TABLE kiosk(
 	id NUMBER CONSTRAINT kiosk_id_pk PRIMARY KEY, -- 키오스크 아이디
 	location VARCHAR2(50) CONSTRAINT kiosk_location_nn NOT NULL, -- 키오스크 위치
-	is_using VARCHAR2(10) CONSTRAINT kiosk_location_nn NOT NULL
-)
+	is_using VARCHAR2(10) CONSTRAINT kiosk_is_using_nn NOT NULL
+);
