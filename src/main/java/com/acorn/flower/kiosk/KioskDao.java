35a0a4fdc3d9a528534a2d9d56a9dbaa376c4614
unso99 <unso99@naver.com>
2024-02-05 21:42:52 +0900

    Add: kiosk table CRUD api 생성
    select, delete, update 생성
    
    Resolves: #4
    Ref: #2

--- src/main/java/com/acorn/flower/kiosk/KioskDao.java
+++ src/main/java/com/acorn/flower/kiosk/KioskDao.java
@@ -1,7 +1,15 @@
 package com.acorn.flower.kiosk;
 
+import java.util.List;
+
 public interface KioskDao {
 	
 	public void insert(KioskDto dto);
-
+	public KioskDto getKiosk(int id);
+	public List<KioskDto> getKioskList();
+	public void delete(int id);
+	public void turnOn(int id);
+	public void turnOff(int id);
+	public void turnOnAll();
+	public void turnOffAll();
 }
--- src/main/java/com/acorn/flower/kiosk/KioskDaoImpl.java
+++ src/main/java/com/acorn/flower/kiosk/KioskDaoImpl.java
@@ -1,5 +1,7 @@
 package com.acorn.flower.kiosk;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -12,6 +14,36 @@ public class KioskDaoImpl implements KioskDao {
 	@Override
 	public void insert(KioskDto dto) {
 		session.insert("kiosk.insert",dto);
+	}
+	@Override
+	public KioskDto getKiosk(int id) {
+		return session.selectOne("kiosk.getKiosk",id);
+	}
+	@Override
+	public List<KioskDto> getKioskList() {
+		return session.selectList("kiosk.getKioskList");
+	}
+	@Override
+	public void delete(int id) {
+		session.delete("kiosk.delete",id);
+	}
+	@Override
+	public void turnOn(int id) {
+		session.update("kiosk.turnOn",id);
+	}
+	@Override
+	public void turnOff(int id) {
+		session.update("kiosk.turnOff",id);
+	}
+	@Override
+	public void turnOnAll() {
+		session.update("kiosk.turnOnAll");
+		
+	}
+	@Override
+	public void turnOffAll() {
+		session.update("kiosk.turnOffAll");
+		
 	}
 
 }
