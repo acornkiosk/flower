26e8b3e5332b7e8b31a7c5e74c0251120b0e0975
이름 <>
2024-02-05 21:28:33 +0900

    Add: #17 상품 삭제기능 생성
    Resolves: #17
    Ref: #10, #12

--- src/main/java/com/acorn/flower/menu/MenuController.java
+++ src/main/java/com/acorn/flower/menu/MenuController.java
@@ -4,6 +4,7 @@ package com.acorn.flower.menu;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -20,10 +21,18 @@ public class MenuController {
 	@Autowired private MenuService service;
 
 	@PostMapping("/menu/update")
-	public String menuUpdate(MenuDto dto) {
+	public String updateMenu(MenuDto dto) {
 		
 		service.updateMenu(dto);
 		
 		return "menu/update";
+	}
+	
+	@GetMapping("/menu/delete")
+	public String deleteMenu(int id) {
+		
+		service.deleteMenu(id);
+		
+		return "menu/delete";
 	}
 }
--- src/main/java/com/acorn/flower/menu/MenuService.java
+++ src/main/java/com/acorn/flower/menu/MenuService.java
@@ -3,5 +3,5 @@ package com.acorn.flower.menu;
 public interface MenuService {
 	public void addMenu(MenuDto dto);
 	public void updateMenu(MenuDto dto);
-	
+	public void deleteMenu(int id);
 }
