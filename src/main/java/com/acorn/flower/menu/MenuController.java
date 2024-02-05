60198487ebd1ca27c43bb5dd045867b3c25743f0
이름 <>
2024-02-05 20:55:29 +0900

    Add: #16 상품 업데이트 기능 생성
    Resolves: #16
    Ref: #10, #12
    Related: #17 삭제기능 미생성

--- src/main/java/com/acorn/flower/menu/MenuController.java
+++ src/main/java/com/acorn/flower/menu/MenuController.java
@@ -7,10 +7,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+/**
+ * React 리팩토링 전 코드, Thymeleaf 전용 메뉴관리 Controller
+ * <br>
+ * @author "이승우", "김대원"
+ * @since 2024-02-05
+ * @version 0.1 / 프로젝트 버전(본인이 임의로) 
+ */
 
 @Controller
 public class MenuController {
 	@Autowired private MenuService service;
 
-	
+	@PostMapping("/menu/update")
+	public String menuUpdate(MenuDto dto) {
+		
+		service.updateMenu(dto);
+		
+		return "menu/update";
+	}
 }
