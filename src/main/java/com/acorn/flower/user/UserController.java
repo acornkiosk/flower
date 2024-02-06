f074c1026c72ee07e2da1b6a9ef13338a9d6b4db
leejunho <kasarotita@naver.com>
2024-02-06 17:30:32 +0900

    "Add: Insert user"
    Insert user 기능 추가함
    
    Resolves : #48
    Ref: #45

--- src/main/java/com/acorn/flower/user/UserController.java
+++ src/main/java/com/acorn/flower/user/UserController.java
@@ -1,21 +1,10 @@
 package com.acorn.flower.user;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.acorn.flower.kiosk.KioskDto;
-
-
-import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class UserController {
