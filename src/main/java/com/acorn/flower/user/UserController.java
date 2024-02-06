a3b55459823ee22fce5c3af4b079af7dfadaafb9
deawon <19999eo@naver.com>
2024-02-06 18:13:56 +0900

    Add: detal testpage 추가
    detail testpage 추가
    
    Resolves:#59
    Ref:#13

--- src/main/java/com/acorn/flower/user/UserController.java
+++ src/main/java/com/acorn/flower/user/UserController.java
@@ -2,12 +2,6 @@ package com.acorn.flower.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-<<<<<<< Updated upstream
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-=======
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,14 +12,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
->>>>>>> Stashed changes
 @RestController
 public class UserController {
 	@Autowired
 	private UserService service;
 	
-<<<<<<< Updated upstream
 	@PostMapping("/api/user/insert")
 	public ResponseEntity<String> UserInset(@RequestBody UserDto dto){
 		service.userInsert(dto);
@@ -33,33 +24,4 @@ public class UserController {
 		return ResponseEntity.ok("id"+ " 님의 회원가입이 성공적으로 완료되었습니다.");
 	}
 	
-=======
-	@PutMapping("/api/user/update")
-	public Map<String, Object> userUpdate(@RequestBody UserDto dto) {
-		Map<String, Object> result = new HashMap<String, Object>();
-		service.userUpdate(dto);
-		UserDto dto2 = service.getUser(dto.getPassword());
-		
-		if(dto2 != null) {
-			result.put("isSuccess", true);
-			result.put("dto", dto2);
-		}else {
-			result.put("isSuccess", false);
-			result.put("dto", null);
-		}
-		return result;
-	}
-	
-	@DeleteMapping("/api/user/{id}")
-	public ResponseEntity<String> deleteUser(@PathVariable String id) {
-		
-		return ResponseEntity.ok("User with ID " + id + " has been deleted successfully.");
-		
-	}
-	
-	
-	
-	
-	
->>>>>>> Stashed changes
 }
