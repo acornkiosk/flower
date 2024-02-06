fcc10b46a4dec9847b4fdd510a01d9017f89578e
deawon <19999eo@naver.com>
2024-02-07 00:55:09 +0900

    Add : menuList, details pages 생성 apiController 수정중
    menuList와 details pages 생성 ,
    apiController 수정중,
    기존 updateform과 합치는 중
    
    Resolves: #32 #59
    Ref:#13 #12

--- src/main/java/com/acorn/flower/menu/MenuController.java
+++ src/main/java/com/acorn/flower/menu/MenuController.java
@@ -1,11 +1,15 @@
 package com.acorn.flower.menu;
 
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -21,9 +25,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class MenuController {
 	@Autowired private MenuService service;
 	
+	
+	@GetMapping("/menu/detail")
+	public String detail(Model model,int id) {
+		Map<String, Object> dataMap = service.getData(id);
+		model.addAttribute("dto",(MenuDto)dataMap.get("dto"));
+	
+		return "menu/detail";
+	}
+	
+
 	@GetMapping("/menu/menu_main")
 	public String MainMenu(Model model) { // 메뉴관리 1번, 관리통합페이지 
-		service.getList(model);
+		List<MenuDto> list=service.getList();
+		model.addAttribute("list",list);
 		return "menu/menu_main";
 	}
 	
@@ -33,19 +48,31 @@ public class MenuController {
 		return "menu/insert_form";
 	}
 	
-	@PostMapping("/menu/insert") // 메뉴관리 2-1 
+	
+	@PostMapping("/menu/insert")// 메뉴관리 2-1 
 	public String insert(MenuDto dto) {
 		service.addMenu(dto);
-		return "menu/menu_main";
-	}
-	
-	@GetMapping("/menu/update_form") // 메뉴관리 3번, 메뉴수정페이지 
-	public String updateForm(Model model, int id) {
-		service.getData(model, id);
-		return "menu/update_form";
+		return "redirect:/menu/menu_main";
 	}
 
-	@PostMapping("/menu/update") // 메뉴관리 3-1 
+	@GetMapping("/menu/update_form") // 메뉴관리 3번, 메뉴수정페이지 
+	public String updateForm(Model model, int id) {
+		Map<String, Object> dataMap = service.getData(id);
+		model.addAttribute("list",dataMap.get("list"));
+		model.addAttribute("dto",dataMap.get("dto"));
+		return "menu/update_form";
+		
+	}
+	/*
+	@GetMapping("/menu/update_form") // 메뉴관리 3번, 메뉴수정페이지 
+	public String updateForm(Model model, int id) {
+		service.getData(id);
+		return "menu/update_form";
+		
+	}*/
+	
+
+	@PostMapping("/menu/update")// 메뉴관리 3-1 
 	public String updateMenu(MenuDto dto) {
 		
 		service.updateMenu(dto);
@@ -60,5 +87,6 @@ public class MenuController {
 		
 		return "menu/delete";
 	}
+
 	
 }
