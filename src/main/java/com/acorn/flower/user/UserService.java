c38b6fd019a41345979bd7efe2b60fa3f98559a1
chan <oyc6739@gmail.com>
2024-02-06 17:42:11 +0900

    "Add: DELETE user"
    updateUser 수정 및 deleteUser 추가함
    Resolve: #50
    Ref: #45

--- src/main/java/com/acorn/flower/user/UserService.java
+++ src/main/java/com/acorn/flower/user/UserService.java
@@ -7,7 +7,13 @@ public interface UserService {
 	public void getInfo(Model model);
 	public void updateUser(UserDto dto);
 	public void updatePassword(UserDto dto);
+<<<<<<< Updated upstream
 	public UserDto getUser(UserDto dto);
 	public UserDto userInsert(UserDto dto);
 	UserDto getUser(String id);
+=======
+	public void userUpdate(UserDto dto);
+	public UserDto getUser(UserDto dto);
+	UserDto getUser(String password);
+>>>>>>> Stashed changes
 }
